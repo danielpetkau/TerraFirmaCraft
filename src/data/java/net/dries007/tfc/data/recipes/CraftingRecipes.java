32bd4fa899e733475a0bfa72c29f4ffa84c57d28
Daniel Petkau <dpetkau@uw.edu>
2025-12-19 15:45:29 -0800

    Fix incorrect smooth mud brick recipe + mud brick tags

--- src/data/java/net/dries007/tfc/data/recipes/CraftingRecipes.java
+++ src/data/java/net/dries007/tfc/data/recipes/CraftingRecipes.java
@@ -1141,8 +1141,8 @@ public interface CraftingRecipes extends Recipes
             .input(TFCTags.Items.GEM_POWDERS)
             .shapeless(TFCItems.SANDPAPER);
         recipe()
-            .input(TFCTags.Items.MUD_BRICK_ITEMS)
             .input(TFCItems.DAUB)
+            .input(TFCTags.Items.MUD_BRICKS)
             .shapeless(TFCBlocks.SMOOTH_MUD_BRICKS);
         recipe()
             .input(TFCItems.GLUE)
